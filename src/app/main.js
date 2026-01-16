85ed6330d55a7ad8ffc8f76ac23bfd1335e7898f
leonardoandrade <leonardo.j.andrade88@gmail.com>
2026-01-16 17:47:13 -0300

    se agrego una pagina de inicio y correcciones de funciones

--- src/app/main.js
+++ src/app/main.js
@@ -2,11 +2,16 @@
 
 const $nameInput = document.querySelector("#name")
 const $emailInput = document.getElementById("email")
+const $contactId = document.getElementById("ID")
 const $form = document.querySelector("form")
+const $btnCancel = document.querySelector("#btn-cancel")
 const $contaactlist = document.querySelector("tbody")
+const $btnClear = document.querySelector("#Clearcontact")
+const $divwelcome = document.querySelector("#bienvenida")
+const $btnStart = document.querySelector("#start")
 
 
-let contacts 
+let contacts
 
 let contactsToLocalStorage = JSON.parse(localStorage.getItem("contacts"))
 
@@ -17,6 +22,13 @@ if (contactsToLocalStorage !== null) {
 }
 
 
+const showwelcome = () => {
+    valid = contacts.length === 0 
+    $divwelcome.style.display = valid? "flex" : "none"
+
+
+}
+
 
 console.log(contacts, "<- la lista de contactos")
 //Creacion de la Funcion (Donde aprende a hacer lo que uno quiere)
@@ -25,81 +37,124 @@ const rendercontacts = () => {
     $contaactlist.innerHTML = ``
 
 
-    for (let i = 0; i < contacts.length ; i++) {
-     // creamos una constante Row, la cual crea una fila de tabla
-     const row = document.createElement("tr")
-     // agregar contenido html
-     row.innerHTML = ` 
+    for (let i = 0; i < contacts.length; i++) {
+        // creamos una constante Row, la cual crea una fila de tabla
+        const row = document.createElement("tr")
+        // agregar contenido html
+        row.innerHTML = ` 
         <td> ${contacts[i].name}</td>
         <td>${contacts[i].email}</td>
         <td>
             <button class = "btn-update" onclick = "updatecontact(${i})"> Actualizar </button>
             <button class = "btn-delete" onclick="deletecontact(${i})" > Borrar </button>
         </td>`
-     // agregar la fila a la pag
-     $contaactlist.appendChild(row)
+        // agregar la fila a la pag
+        $contaactlist.appendChild(row)
     }
+    $contactId.value = ""
+    localStorage.setItem("contacts", JSON.stringify(contacts))
+            $form.reset()
+
 }
 
 
 
 const sendForm = (event) => {
+
+
     event.preventDefault()
     console.log(event)
     console.log("Agregando El valor del nombre es: ", $nameInput.value)
-    console.log ("El valor del email es: ", $emailInput.value)
+    console.log("El valor del email es: ", $emailInput.value)
+
+    const name = $nameInput.value
+    const mail = $emailInput.value
 
 
-    //construir un objeto vinculado con una propiedad y valor
-    const newContact = {
-        name: $nameInput.value, 
-        email: $emailInput.value
+    const dataContac = {
+        name: name,
+        email: mail
+    }
+    if (name === "" || mail === "") {
+        alert("✋Debes completar los campos⛔")
+        return
     }
 
-    // mostrar el ojeto del contacto ( con las clave y valores)
-    console.log (newContact)
+    const foundContact = contacts.find(contacts => contacts.email === mail)
 
-    // adherir a la lista los nuevos contactos 
-    // meter el objeto en la lista de contactos
+    if (foundContact) {
+          alert("✋El email se encuentra regsitrado, intenta con otro⛔")
+        return
+    }
 
-    contacts.push(newContact)
 
-    // Construir datos para navegador de internet para almacenar en localStorage
-    const contactsToJson = JSON.stringify(contacts)
-    // Navegador guarda la listade contactos atualizada
+    if ($contactId.value === "") {
 
-    localStorage.setItem("contacts", contactsToJson)
+        // adherir a la lista los nuevos contactos 
+        // meter el objeto en la lista de contactos
 
-    //mostramos la lista actualizada
+        contacts.push(dataContac)
 
-    console.log(contacts)
 
-    // limpiar el formulario
-    $form.reset()
+        // limpiar el formulario
+        $form.reset()
 
-    //Renderizar los contactos (mostrarlos en la pag)
-    rendercontacts()
+        //Renderizar los contactos (mostrarlos en la pag)
+        rendercontacts()
+    } else {
+        contacts[$contactId.value] = dataContac
+        rendercontacts()
 
+    }
 
 }
 
-const deletecontact = (index) => {
-    console.log (index, "Borrando un contacto accion")
-    contacts.splice(index,1)
-    console.log(contacts)
-    localStorage.setItem("contacs",JSON.stringify(contacts))
-    rendercontacts()
 
+
+const deletecontact = (index) => {
+    const validDel = confirm("¿Estas seguro que quieres borrar el contacto?")
+    if (validDel) {
+        console.log(index, "Borrando un contacto accion")
+        contacts.splice(index, 1)
+        console.log(contacts)
+        rendercontacts()
+    }
 }
 
 const updatecontact = (index) => {
     $nameInput.value = contacts[index].name
     $emailInput.value = contacts[index].email
-   
+    $contactId.value = index
 }
 
+const cancelform = () => {
+    $form.reset()
+}
+
+const clearAll = () => {
+   const confirmclear=  confirm("Estas seguro de querer borrar todos los contactos?")
+   if (confirmclear) {
+    localStorage.removeItem("contacts")
+    contacts = []
+    rendercontacts()
+   }
+}
+
+const startContact = () => {
+    $divwelcome.style.display = "none"
+    console.log("deberia ocultarse la pagina de inicio")
+}
+
+
+
+$btnCancel.addEventListener("click", cancelform)
 // Al formulario dejarlo en escucha hasta que suceda un evento
 // en este caso el evento es enviar, y cuando suceda se ejecute una funcion
 //Invocacion de un evento y de la funcion
-$form.addEventListener("submit", sendForm )
-    rendercontacts()
+$form.addEventListener("submit", sendForm)
+
+$btnClear.addEventListener("click",clearAll)
+
+$btnStart.addEventListener("click", startContact)
+showwelcome()
+rendercontacts()
